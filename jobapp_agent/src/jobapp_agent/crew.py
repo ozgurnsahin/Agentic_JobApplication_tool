ebb2c40879ccf64afe0c7215c2b6bddfa23d31db
Özgür Nazım <nazm.albatros@gmail.com>
2025-06-17 23:02:25 +0300

    feat: added pdf search to extract key search criterias

--- jobapp_agent/src/jobapp_agent/crew.py
+++ jobapp_agent/src/jobapp_agent/crew.py
@@ -2,11 +2,12 @@ from .db.database import CrewAIJobStorage
 from .tools.job_database_tool import JobDatabaseTool 
 
 from typing import List
+import os
 
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
-from crewai_tools import SerperDevTool, PGSearchTool
+from crewai_tools import SerperDevTool, PGSearchTool, PDFSearchTool
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -20,6 +21,8 @@ class JobappAgent():
     @agent
     def researcher(self) -> Agent:
         db = CrewAIJobStorage()
+        cv_path = os.path.join(os.path.dirname(__file__), "..", "..", "knowledge", "ozgur_cv.pdf")
+        cv_path = os.path.abspath(cv_path)
         
         return Agent(
             config=self.agents_config['researcher'],
@@ -30,7 +33,10 @@ class JobappAgent():
             max_reasoning_attempts=3,
             max_iter=10,
             max_max_execution_time=3600,
-            tools=[SerperDevTool(country="Turkey", n_results=15),PGSearchTool(db_uri=db.connection_url,table_name='jobs'),JobDatabaseTool()],
+            tools=[SerperDevTool(country="Turkey", n_results=15),
+                   PGSearchTool(db_uri=db.connection_url,table_name='jobs'),
+                   JobDatabaseTool(),
+                   PDFSearchTool(pdf=cv_path)],
             respect_context_window=True
         )
 
