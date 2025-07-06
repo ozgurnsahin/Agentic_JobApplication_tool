459cc9c36b89ff2a6986df37879eb8b1f823575d
Özgür Nazım <nazm.albatros@gmail.com>
2025-07-06 13:53:22 +0300

    fix: tool and crew fixes

--- jobapp_agent/src/jobapp_agent/crew.py
+++ jobapp_agent/src/jobapp_agent/crew.py
@@ -1,6 +1,5 @@
 from .db.database import CrewAIJobStorage
 from .tools.job_database_tool import JobDatabaseTool 
-# from .tools.duckduckgo_tool import DuckDuckGoTool
 
 from typing import List
 import os
@@ -50,7 +49,6 @@ class JobappAgent():
             verbose=True,
             inject_date=True,
             date_format="%d-%m-%Y",
-            # reasoning=True,
             max_iter=15,
             max_max_execution_time=3600,
             tools=[PGSearchTool(db_uri=db.connection_url,table_name='jobs'),
